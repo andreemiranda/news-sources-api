8a3cc7d4e8dfc83bcf64a9917ae5426a328953d7
André Miranda <39061067+andreemiranda@users.noreply.github.com>
2026-08-17 23:05:43 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  poweredByHeader: false,
-  compress: true,
-  reactStrictMode: true,
-  productionBrowserSourceMaps: false,
-};
-
-module.exports = nextConfig;
